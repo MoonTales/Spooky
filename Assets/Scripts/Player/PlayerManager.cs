603e1fbdd1e3f5bc5c2eac7d43ab5a778013eaed
MoonTales <CohenLCalvert@gmail.com>
2026-01-10 14:32:12 -0700

    small update to allow changing the spawn anchor we are spawning at

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -11,7 +11,7 @@ namespace Player
     public class PlayerManager : Singleton<PlayerManager>
     {
         private GameObject _player;
-        private 
+        [SerializeField] private string _defaultSpawnPointID = "DEFAULT_SPAWN_POINT";
         
         
         void Start()
@@ -46,7 +46,7 @@ namespace Player
                     FirstAnchor = Anchor;
                 }
                 // Search for the default spawn point ID
-                if (Anchor != null && Anchor.gameObject != null && Anchor.GetSpawnPointID() == "DEFAULT_SPAWN_POINT")
+                if (Anchor != null && Anchor.gameObject != null && Anchor.GetSpawnPointID() == _defaultSpawnPointID)
                 {
                     // found the default spawn point, move the player here
                     if (_player != null)
