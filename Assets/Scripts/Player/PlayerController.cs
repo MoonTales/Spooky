05e26ae4df411ce5708d8afa51672aff4ebcea66
MoonTales <CohenLCalvert@gmail.com>
2026-01-11 09:47:45 -0700

    Initial player template setup (just the camera LOL)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,7 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Types = System.Types;
 
 namespace Player
@@ -10,9 +12,71 @@ namespace Player
     /// </summary>
     public class PlayerController : EventSubscriberBase
     {
+        
+        [Header("Movement Settings")]
+        [SerializeField] private float walkSpeed = 5.0f;
+        [SerializeField] private float sprintSpeed = 8.0f;
+        [SerializeField] private float crouchSpeed = 2.0f;
+        [Space(10)]
+        [Header("References (try to remove these")]
+        [SerializeField] private Transform cameraTransform;
+        [SerializeField] private InputActionReference moveAction;
+        private CharacterController _characterController;
+        private Vector2 _moveInput;
+        
         // Local reference that the controller cares about
         [SerializeField] private Types.PlayerHealthState currentPlayerHealthState;
 
+
+        private void Awake()
+        {
+            // set up initial character variables
+            _characterController = GetComponent<CharacterController>();
+            // look through all of the children of this player, to find an object with the cinemachine camera component
+            foreach (Transform child in transform)
+            {
+                if (child.GetComponentInChildren<CinemachineCamera>() != null)
+                {
+                    cameraTransform = child;
+                    DebugUtils.LogSuccess("PlayerController: Found Cinemachine Camera in child object: " + child.name);
+                    break;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            
+            moveAction.action.performed += OnMovePerformed;
+            moveAction.action.canceled += OnMovePerformed;
+        }
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            
+            moveAction.action.performed -= OnMovePerformed;
+            moveAction.action.canceled -= OnMovePerformed;
+        }
+
+        private void OnMovePerformed(InputAction.CallbackContext obj)
+        {
+            _moveInput = obj.ReadValue<Vector2>();
+        }
+
+        private void HandleMovement()
+        {
+            
+            Vector3 moveDirection = cameraTransform.TransformDirection(new Vector3(_moveInput.x, 0, _moveInput.y)).normalized;
+            float currentSpeed = walkSpeed;
+            Vector3 velocity = moveDirection * currentSpeed;
+            _characterController.Move(velocity * Time.deltaTime);
+        }
 
         protected override void OnGameStateChanged(Types.GameState newState)
         {
