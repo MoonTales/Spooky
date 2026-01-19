e53cd9a01644047939bd8ffb1427857981441745
Xenocynic <danktangent.mxc@gmail.com>
2026-01-19 16:57:29 -0700

    interactor + edits to interface

--- Assets/Scripts/Interaction/IInteractable.cs
+++ Assets/Scripts/Interaction/IInteractable.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public interface IInteractable
 {
-    // maybe a short prompt for UI, like "pick up key", "hide", "turn off alarm". Can be removed if not needed.
+    // maybe a short prompt for UI, like "pick up key", "hide", "turn off alarm"
     string Prompt { get; }
+
 
     // return false to block interaction (locked, already used, wrong state, whatever)
     // pass Interactor here just in case enemies interact with objects differently
@@ -11,4 +12,10 @@ public interface IInteractable
 
     // called when the player interacts
     void Interact(Interactor interactor);
+
+
+    // optional common audio (can be null)
+    AudioClip HoverSfx { get; }
+    AudioClip InteractSfx { get; }
+    AudioClip DeniedSfx { get; }
 }
--- Assets/Scripts/Interaction/Interactor.cs
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float castDistance = 5f;
+    [SerializeField] private Vector3 raycastOffset = new Vector3(0f, 1f, 0f);
+    [SerializeField] private KeyCode interactKey = KeyCode.E;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(interactKey))
+        {
+            if (TryGetInteractable(out IInteractable interactable))
+            {
+                if (interactable.CanInteract(this))
+                {
+                    interactable.Interact(this);
+                }
+                else
+                {
+                    // play "denied" sound or show "locked" UI here.
+                    // Debug.Log("Can't interact.");
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool TryGetInteractable(out IInteractable interactable)
     {
-        
+        interactable = null;
+
+        Ray ray = new Ray(transform.position + raycastOffset, transform.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, castDistance))
+        {
+            interactable = hitInfo.collider.GetComponent<IInteractable>();
+
+            return interactable != null;
+        }
+
+        return false;
     }
 }
