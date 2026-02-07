761d33064ea20652c54aae9e264e67f2b05b7d12
ParalyzedMoth <85903951+ParalyzedMoth@users.noreply.github.com>
2026-02-07 01:33:26 -0700

    Infinite Hallway Works Completely

--- Assets/TeleportPlayer.cs
+++ Assets/TeleportPlayer.cs
@@ -4,12 +4,23 @@ using Player;
 public class TeleportPlayer : MonoBehaviour
 {
 	[SerializeField] private Vector3 teleportLocation;
+	[SerializeField] private bool keepPlayerX = false;
+	[SerializeField] private bool keepPlayerY = false;
+	[SerializeField] private bool keepPlayerZ = false;
+	private Transform playerReference;
+
+	private void Start()
+	{
+		playerReference = PlayerManager.Instance.GetPlayer().transform;
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			PlayerManager.Instance.TeleportPlayer(teleportLocation);
+			Vector3 realLocation = new Vector3(keepPlayerX ? playerReference.position.x : teleportLocation.x, keepPlayerY ? playerReference.position.y :
+				teleportLocation.y, keepPlayerZ ? playerReference.position.z : teleportLocation.z);
+			PlayerManager.Instance.TeleportPlayer(realLocation);
 		}
 	}
 }
