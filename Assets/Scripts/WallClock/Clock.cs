8283bfd6c14835259f084d667f6d59c4096e6405
emery-collab <therouxemery@gmail.com>
2026-02-21 22:20:43 -0700

    fixed clock, implemented anxiety

--- Assets/Scripts/WallClock/Clock.cs
+++ Assets/Scripts/WallClock/Clock.cs
@@ -16,28 +16,32 @@ public class Clock : MonoBehaviour
     //public float rotateSpeed;
     public GameObject minHand;
     public GameObject hourHand;
-    public float timeToExit = 600f;
-    public float FastForwardSpeed = 3f;
     private float hourHandDegPerSec;
     private float minuteHandDegPerSec;
-    private float elapsedTime;
-    private bool _isInspecting;
-    private float ClockSpeed;
+    //private bool _isInspecting;
+    private Types.GameState _currentGameState;
+    [SerializeField] private float timeToExit = 660f;
+    [SerializeField] private float FastForwardSpeed = 3f;
+    [SerializeField] private float elapsedTime;
+    [SerializeField] private float ClockSpeed;
+    [SerializeField] private float damagePerTick;
 
     void Start()
     {
+        // Initialize GameState
+        _currentGameState = GameStateManager.Instance.GetCurrentGameState();
+        // Deprecated time calculation for 14 hrs.
         // 14 hours = 14 * 30° = 420°
-        // 10 minutes = 600 seconds    
-        ClockSpeed = 420f / 600f * 2;    
-        minuteHandDegPerSec = ClockSpeed * Time.deltaTime;  // 8.4° per second
-        hourHandDegPerSec = minuteHandDegPerSec / 12;   // 0.7° per second   
+        // 10 minutes = 600 seconds // 8.4° per second minute hand   // 0.7° per second hour hand
+        // New time calculation for 12 hrs
+        // 1 minute = 6° per second, 1 hour = 6/12 = 0.5° per second
+        ClockSpeed = 6f;
+        damagePerTick = 100/timeToExit;  // Normalized damage per second to player sanity
+        minuteHandDegPerSec = ClockSpeed; // 6° per second for minute hand
+        hourHandDegPerSec = minuteHandDegPerSec / 12; // 0.5° per second for hour hand
         StartCoroutine(Timer());
         StartCoroutine(ClockTick());
 
-    }
-    void Update()
-    {
-        
     }
 
     IEnumerator Timer()
@@ -45,15 +49,19 @@ public class Clock : MonoBehaviour
         while (elapsedTime < timeToExit)
         {
             yield return new WaitForSeconds(1f);
-            if (_isInspecting)
+            if (_currentGameState != Types.GameState.Paused)
             {
-                elapsedTime = elapsedTime + FastForwardSpeed;
+                if (_currentGameState == Types.GameState.Inspecting)
+                {
+                    elapsedTime = elapsedTime + FastForwardSpeed;
+                    EventBroadcaster.Broadcast_OnPlayerDamaged(damagePerTick*FastForwardSpeed);
+                }
+                else
+                {
+                    elapsedTime++;
+                    EventBroadcaster.Broadcast_OnPlayerDamaged(damagePerTick);
+                }
             }
-            else
-            {
-                elapsedTime++;
-            }
-            
         }
     }
     IEnumerator ClockTick()
@@ -61,17 +69,22 @@ public class Clock : MonoBehaviour
         
         while (elapsedTime < timeToExit)
         {
-            _isInspecting = PlayerController.Instance.IsPlayerInspecting();
-            if (_isInspecting)
+            //_isInspecting = PlayerController.Instance.IsPlayerInspecting();
+            _currentGameState = GameStateManager.Instance.GetCurrentGameState();
+            if (_currentGameState != Types.GameState.Paused)
             {
-                minHand.transform.Rotate(0, 0, -minuteHandDegPerSec*FastForwardSpeed, Space.World);
-                hourHand.transform.Rotate(0, 0, -hourHandDegPerSec*FastForwardSpeed, Space.World);
+                if (_currentGameState == Types.GameState.Inspecting)
+                {
+                    minHand.transform.Rotate(0, 0, -minuteHandDegPerSec * Time.deltaTime * FastForwardSpeed, Space.World);
+                    hourHand.transform.Rotate(0, 0, -hourHandDegPerSec * Time.deltaTime * FastForwardSpeed, Space.World);
+                }
+                else
+                {
+                    minHand.transform.Rotate(0, 0, -minuteHandDegPerSec * Time.deltaTime, Space.World);
+                    hourHand.transform.Rotate(0, 0, -hourHandDegPerSec * Time.deltaTime, Space.World);
+                }
             }
-            else
-            {
-                minHand.transform.Rotate(0, 0, -minuteHandDegPerSec, Space.World);
-                hourHand.transform.Rotate(0, 0, -hourHandDegPerSec, Space.World);
-            }
+            // Pause for 1 frame
             yield return null;
         }
         // we are good to sleep!
