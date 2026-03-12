353092d0b1819b34601d2fb58a50e3d13eb86326
Cohen <CohenLCalvert@gmail.com>
2026-03-12 13:08:34 -0600

    weeeeeee

--- Assets/Scripts/Managers/LetterManager.cs
+++ Assets/Scripts/Managers/LetterManager.cs
@@ -89,9 +89,6 @@ namespace Managers
             else if(id == "res_letter_2"){ _hasReadAct2ResearcherLetter = true;}
             else if(id == "fren_letter_3"){ _hasReadAct3FriendLetter = true;}
             else if(id == "res_letter_3"){ _hasReadAct3ResearcherLetter = true;}
-            
-            //SaveSystem.Instance.RequestSave(this);
-            // we have a special edge case for the fren_letter_4, as that will transfer us to the finale
         }
 
 
