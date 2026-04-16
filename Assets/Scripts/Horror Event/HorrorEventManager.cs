f8d6f9f15ae7747a4ac3073ac5eac7b358d2c58d
MoonTales <CohenLCalvert@gmail.com>
2026-04-16 17:19:11 -0600

    saving works (still not as good as I want)

--- Assets/Scripts/Horror Event/HorrorEventManager.cs
+++ Assets/Scripts/Horror Event/HorrorEventManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Horror_Event;
 using UnityEngine;
 
@@ -7,9 +8,10 @@ public class HorrorEventManager : Singleton<HorrorEventManager>, ISaveSystemInte
 {
     public struct HorrorEventSaveData
     {
-        public Hashtable activeNotifications;
+        public List<string> triggeredEventIds;
     }
-    private Hashtable _activeNotifications = new Hashtable();
+
+    private HashSet<string> _activeNotifications = new HashSet<string>();
 
     
     protected override void RegisterSubscriptions()
@@ -22,33 +24,25 @@ public class HorrorEventManager : Singleton<HorrorEventManager>, ISaveSystemInte
 
     private void OnHorrorEventTriggered(HorrorEvent data)
     {
-        // check if this object has already been fired, and in our hashtable, if it has, we should ignore it.
-        // we will key based on the message override (for now)
-        // if we have a valid text key, use the TextKey.id as they key
         string notificationKey = data.GetHorrorEventId();
-        
-        if (_activeNotifications.ContainsKey(notificationKey)) { return; }
-        _activeNotifications.Add(notificationKey, true);
+        if (!_activeNotifications.Add(notificationKey)) { return; } 
         DebugUtils.LogSuccess("HorrorEventManager registered a new horror event trigger: " + notificationKey);
     }
     
     public bool CheckIfHorrorEventTriggered(HorrorEvent horrorEvent)
     {
-        string notificationKey = horrorEvent.GetHorrorEventId();
-        return _activeNotifications.ContainsKey(notificationKey);
+        return _activeNotifications.Contains(horrorEvent.GetHorrorEventId());
     }
 
     public string SaveId => "HorrorEventManager";
     public HorrorEventSaveData OnSave()
     {
-        return new HorrorEventSaveData()
-        {
-            activeNotifications = _activeNotifications
-        };
+        return new HorrorEventSaveData { triggeredEventIds = new List<string>(_activeNotifications) };
+
     }
 
     public void OnLoad(HorrorEventSaveData data)
     {
-        _activeNotifications = data.activeNotifications;
+        _activeNotifications = new HashSet<string>(data.triggeredEventIds);
     }
 }
